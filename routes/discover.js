adf837f75d8515ae88ba9726c645dc3f87cbd3f6
Farid-SolaradAi <farid.ahmed@solarad.ai>
2024-06-03 19:01:07 +0530

    fix discover.js

--- routes/discover.js
+++ routes/discover.js
@@ -10,88 +10,66 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-route.get("/folder/create/:foldername/:email", async (req, res, next) => {
+// Create a new folder
+route.post("/folder/create", async (req, res, next) => {
     try {
-        const folderName = req.params.foldername;
-        const email = req.params.email;
-        const newFolder = { folderName: folderName, email: email };
+        const { foldername, email } = req.body;
+        const newFolder = { folderName: foldername, email: email };
         const result = await Folder.create(newFolder);
-        if (result) res.send("Folder Created Successfully");
-        else res.send("Folder Creation Unsuccessful!");
-    }
-    catch (err) {
+        if (result) res.status(201).send("Folder Created Successfully");
+        else res.status(400).send("Folder Creation Unsuccessful!");
+    } catch (err) {
         next(err);
     }
 });
 
-route.get("/getFolders/:email", (req, res, next) => {
+// Get folders by email
+route.get("/folders/:email", async (req, res, next) => {
     try {
         const email = req.params.email;
-        Folder.find({ email: { $ne: null, $eq: email } }, (err, data) => {
-            if (err) {
-                console.log(err);
-                res.status(500).send(err);
-            } else {
-                res.status(200).send(data);
-            }
-        });
+        const folders = await Folder.find({ email: email });
+        res.status(200).send(folders);
     } catch (err) {
-        console.log(err);
         next(err);
     }
 });
 
-route.get("/file/create", async (req, res, next) => {
+// Create a new file
+route.post("/file/create", async (req, res, next) => {
     try {
-        const fileName = req.query.fileName;
-        const folderName = req.query.folderName;
-        const fileData = req.query.fileData;
-        const email = req.query.email;
+        const { fileName, folderName, fileData, email } = req.body;
 
         await File.findOneAndDelete({ fileName: fileName, email: email });
         const newFile = { fileName: fileName, folderName: folderName, fileData: fileData, email: email };
         const result = await File.create(newFile);
 
-        if (result) res.send("Successful");
-        else res.send("Unsuccessful");
-
-    }
-    catch (err) {
-        console.log(err);
+        if (result) res.status(201).send("File Created Successfully");
+        else res.status(400).send("File Creation Unsuccessful!");
+    } catch (err) {
         next(err);
     }
 });
 
-route.get("/getFiles/:folderName/:email", async (req, res, next) => {
+// Get files by folder name and email
+route.get("/files/:folderName/:email", async (req, res, next) => {
     try {
-        const folderName = req.params.folderName;
-        const email = req.params.email;
-        if (folderName) {
-            File.find({ folderName: folderName, email: { $ne: null, $eq: email } }, (err, data) => {
-                if (err) console.log(err);
-                else { res.status(200).send(data); };
-            });
-        }
-    }
-    catch (err) {
-        console.log(err);
+        const { folderName, email } = req.params;
+        const files = await File.find({ folderName: folderName, email: email });
+        res.status(200).send(files);
+    } catch (err) {
         next(err);
     }
 });
 
-route.get("/getFiles", async (req, res, next) => {
+// Get all files by email
+route.get("/files", async (req, res, next) => {
     try {
-        const email = req.params.email
-        File.find({ email: { $ne: null, $eq: email } }, (err, data) => {
-            if (err) console.log(err);
-            else { res.status(200).send(data); };
-        });
-    }
-    catch (err) {
-        console.log(err);
+        const email = req.query.email;
+        const files = await File.find({ email: email });
+        res.status(200).send(files);
+    } catch (err) {
         next(err);
     }
 });
-
 
 module.exports = route;
